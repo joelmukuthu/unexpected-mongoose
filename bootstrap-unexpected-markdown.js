76f9bbb650eecc90aef464949972e56918f72684
Joel Mukuthu <joelmukuthu@gmail.com>
2018-02-14 22:15:08 +0100

    chore: add mongodb

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,4 +1,6 @@
-/*global unexpected:true*/
+/*global unexpected:true, mongoose:true*/ // eslint-disable-line no-unused-vars
 unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
 unexpected.use(require('./lib/unexpected-mongoose'));
+
+mongoose = require('mongoose');
