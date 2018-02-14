ba7c51995c3ede89de5203e647ddd5d42fab99e5
Joel Mukuthu <joelmukuthu@gmail.com>
2018-02-14 22:29:17 +0100

    test(ModelArray): add failing test for 'to satisfy ModelArray'

--- test/unexpected-mongoose.spec.js
+++ test/unexpected-mongoose.spec.js
@@ -1,2 +1,28 @@
-describe('unexpected-mongoose', function() {
+const mongoose = require('mongoose');
+const expect = require('unexpected').use(require('../lib/unexpected-mongoose'));
+
+describe('unexpected-mongoose', () => {
+  let Cat;
+
+  before(async () => {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connection.dropDatabase();
+    Cat = mongoose.model('Cat', { name: String });
+  });
+
+  describe('ModelArray', () => {
+    describe('to satisfy ModelArray', () => {
+      let cats;
+
+      before(async () => {
+        cats = await Cat.insertMany([{ name: 'foo' }, { name: 'bar' }]);
+      });
+
+      it('loops infinitely', async () =>
+        expect(cats, 'to satisfy', [
+          new Cat({ name: 'foo' }),
+          new Cat({ name: 'bar' })
+        ]));
+    });
+  });
 });
